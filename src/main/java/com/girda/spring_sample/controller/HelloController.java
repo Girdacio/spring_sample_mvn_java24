12a72c2e05761cfc9c2f5cb882a2af649372a4f0
Girdacio <girdacio@gmail.com>
2025-05-18 23:31:44 -0300

    log using logger

--- src/main/java/com/girda/spring_sample/controller/HelloController.java
+++ src/main/java/com/girda/spring_sample/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.girda.spring_sample.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LogManager.getLogger(HelloController.class);
     
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("Rota /hello acessada");
-        System.out.println("Debug para diagnóstico");
+        logger.info("Rota /hello acessada");
+        logger.debug("Debug para diagnóstico");
         return "Olá, mundo!";
     }
 }
